cdd89dee30f70adbd2aec4a4b5bd142c1ee6636a
jahidexe <46343754+jahidexe@users.noreply.github.com>
2025-08-09 23:12:28 +0100

    keeping api

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,5 @@
-
+resource "google_project_service" "bigquery" {
+  project              = var.project_id
+  service              = "bigquery.googleapis.com"
+  disable_on_destroy   = false   # <— IMPORTANT
+}
