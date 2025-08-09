50e4fdb85077f455a68fbd0751d2e2667e285b6e
jahidexe <46343754+jahidexe@users.noreply.github.com>
2025-08-09 19:52:56 +0100

    bq module not working

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,23 +3,6 @@ resource "google_project_service" "bigquery" {
   service = "bigquery.googleapis.com"
 }
 
-module "bq_bronze" {
-  source  = "terraform-google-modules/bigquery/google//modules/dataset"
-  version = "~> 10.0"
-
-  project_id = var.project_id
-  dataset_id = "bronze"
-  location   = var.bq_location
-
-  delete_contents_on_destroy = true
-  labels = {
-    env   = var.env
-    layer = "bronze"
-  }
-
-  depends_on = [google_project_service.bigquery]
-}
-
 
 module "bq_bronze" {
   source                     = "github.com/terraform-google-modules/terraform-google-bigquery//modules/dataset?ref=v10.1.1"
