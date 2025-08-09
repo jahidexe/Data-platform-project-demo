58f6d5711e93515d073603b2df5e71aeb6c2d94e
jahidexe <46343754+jahidexe@users.noreply.github.com>
2025-08-09 18:36:26 +0100

    removing dupicate

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "tf-backend-esg-dev-jahid" # <-- ensure this bucket exists
-    prefix = "state"
+    bucket = "tf-backend-esg-dev-jahid"
+    prefix = "data-platform-project-demo/state"
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.30"
-    }
-  }
-}
-
-
 provider "google" {
   project = var.project_id
   region  = var.region
