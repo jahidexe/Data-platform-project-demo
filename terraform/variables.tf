b86d157881382bb398a549513085ad2bcb2acc91
jahidexe <46343754+jahidexe@users.noreply.github.com>
2025-08-09 19:43:51 +0100

    Added modules, updated workflow etx

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,24 @@
+
 variable "project_id" {
   description = "GCP project ID"
   type        = string
   default     = "esg-dev-jahid"
 }
+
 variable "region" {
   description = "Default region for regional resources"
   type        = string
   default     = "europe-west2"
 }
+
 variable "bq_location" {
   description = "BigQuery dataset location"
   type        = string
   default     = "EU"
+}
+
+variable "env" {
+  description = "Deployment environment label"
+  type        = string
+  default     = "dev"
 }
